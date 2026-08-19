a26891f2a6067582bccce7aebdee9ea27829ee64
gelbysik4725-alt <gelbysik4725@gmail.com>
2026-08-19 15:23:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ error_reporting(0);
 date_default_timezone_set('Europe/Moscow');
 
 // ====================== НАСТРОЙКИ ======================
-$botToken   = getenv('BOT_TOKEN') ?: '8883380357:AAHrYtiqhcCTBvllozb5m4pMUQIw922a0Oo';
-$adminId    = (int)(getenv('ADMIN_ID') ?: 8875180956);
-$adminPass  = getenv('ADMIN_PASS') ?: 'admin123'; // пароль для веб-админки
+$botToken   = getenv('8883380357:AAHrYtiqhcCTBvllozb5m4pMUQIw922a0Oo') ?: '';
+$adminId    = (int)(getenv('8875180956') ?: );
+$adminPass  = getenv('admin123') ?: 'admin123'; // пароль для веб-админки
 
 if (empty($botToken) || empty($adminId)) {
     http_response_code(500);
